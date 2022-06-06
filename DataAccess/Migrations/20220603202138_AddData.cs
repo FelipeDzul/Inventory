caae2b2864fb8066acf13af95409d0cd594d00af
Felipe Dzul <dual.felipe.dzul@gmail.com>
2022-06-06 09:57:33 -0500

    Repositorio del proyecto desarrollado en el curso de desarrollo web con Blazor y .Net

---
+++ DataAccess/Migrations/20220603202138_AddData.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class AddData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Warehouses",
+                keyColumn: "WarehouseId",
+                keyValue: "c7a30e8a-0027-43a9-9026-b8bb67ef6259");
+
+            migrationBuilder.DeleteData(
+                table: "Warehouses",
+                keyColumn: "WarehouseId",
+                keyValue: "d9e90215-ecf0-4d72-a447-37705d2e1a16");
+
+            migrationBuilder.InsertData(
+                table: "Warehouses",
+                columns: new[] { "WarehouseId", "WarehouseAddress", "WarehouseName" },
+                values: new object[] { "c7014cdf-b38f-4eba-ad6f-0cd006d1dc86", "Calle 8 con 23", "Bodega Central" });
+
+            migrationBuilder.InsertData(
+                table: "Warehouses",
+                columns: new[] { "WarehouseId", "WarehouseAddress", "WarehouseName" },
+                values: new object[] { "94123428-a730-4e17-bc46-387eab029381", "Calle norte con occidente", "Bodega Norte" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Warehouses",
+                keyColumn: "WarehouseId",
+                keyValue: "94123428-a730-4e17-bc46-387eab029381");
+
+            migrationBuilder.DeleteData(
+                table: "Warehouses",
+                keyColumn: "WarehouseId",
+                keyValue: "c7014cdf-b38f-4eba-ad6f-0cd006d1dc86");
+
+            migrationBuilder.InsertData(
+                table: "Warehouses",
+                columns: new[] { "WarehouseId", "WarehouseAddress", "WarehouseName" },
+                values: new object[] { "d9e90215-ecf0-4d72-a447-37705d2e1a16", "Calle 8 con 23", "Bodega Central" });
+
+            migrationBuilder.InsertData(
+                table: "Warehouses",
+                columns: new[] { "WarehouseId", "WarehouseAddress", "WarehouseName" },
+                values: new object[] { "c7a30e8a-0027-43a9-9026-b8bb67ef6259", "Calle norte con occidente", "Bodega Norte" });
+        }
+    }
+}
